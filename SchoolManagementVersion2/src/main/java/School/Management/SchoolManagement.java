16abecf9c5d648221dfe436914e0d839dffcd199
WasanMoosa <wasssoon@gmail.com>
2023-01-03 09:49:26 +0400

    need to let the code not enable to register same student again

--- SchoolManagementVersion2/src/main/java/School/Management/SchoolManagement.java
+++ SchoolManagementVersion2/src/main/java/School/Management/SchoolManagement.java
@@ -6,11 +6,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
+
+import javax.swing.tree.AbstractLayoutCache;
+
 import java.lang.reflect.Array;
 import java.lang.reflect.Type;
 import com.google.gson.reflect.TypeToken;
@@ -24,26 +28,90 @@ import com.google.gson.GsonBuilder;
 public class SchoolManagement {
 
 	public static void main(String[] args) {
+		HashMap<String, String[]> allStudent = new HashMap();
 		Scanner sc = new Scanner(System.in);
+		// Loading the file
+
+		File myFile = new File("student.json");
+		if (myFile.exists()) {
+			Gson gson = new GsonBuilder().create();
+			HashMap<String, String[]> temp = new HashMap();
+
+			Scanner sfile;
+			try {
+				sfile = new Scanner(myFile);
+				while (sfile.hasNextLine()) {
+					String st;
+					st = sfile.nextLine();
+					Type type = new TypeToken<HashMap<String, String[]>>() {
+					}.getType();
+					temp = gson.fromJson(st, type);
+					allStudent.putAll(temp);
+				}
+
+			} catch (FileNotFoundException e) {
+
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				myFile.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 		// Showing the menu
 		menu();
 
 		// choosing the action
-		int adm = Integer.parseInt(sc.nextLine());
+		int action = Integer.parseInt(sc.nextLine());
 		System.out.println("\n");
 		// Registeration method
-		if (adm == 1) {
+		if (action == 1) {
 			registration();
 		}
-		if (adm == 2) {
-			String id;
-			System.out.println("Do you want to search for student using");
-			System.out.println("1) name");
-			System.out.println("2) id");
-			id = sc.nextLine();
-			Searching(id);
 
+		if (action == 2) {
+			String idSearch;
+			System.out.println("Enter the id of the student to search for it");
+			idSearch = sc.nextLine();
+			Searching(idSearch, allStudent);
+		}
+
+		// Manage student
+		if (action == 3) {
+			System.out.println("Choose the action");
+			System.out.println("1) Delete Student");
+			System.out.println("2) Update Student");
+			int manageAction = Integer.parseInt(sc.nextLine());
+
+			if (manageAction == 1) {
+				// Deleting
+				boolean continueDelet = true;
+				while (continueDelet) {
+					System.out.println("Enter the id of the student to delet it");
+					String idDelet = sc.nextLine();
+					delete(idDelet, allStudent);
+					System.out.println("Do you want to delet more?");
+					System.out.println("Delet next student (press 1)");
+					System.out.println("Finish (press 0) ");
+
+					if (Integer.parseInt(sc.nextLine()) == 1) {
+						continueDelet = true;
+					} else {
+						continueDelet = false;
+
+					}
+
+				}
+			}
+			if (manageAction == 2) {
+				System.out.println("Enter the id of the student to update it");
+				String idUpdate = sc.nextLine();
+				update(idUpdate, allStudent);
+			}
 		}
 
 	}
@@ -62,8 +130,23 @@ public class SchoolManagement {
 
 	public static void putInFile(HashMap<String, String[]> studentRegistered) {
 		File myFile = new File("student.json");
-
 		try (FileWriter writer = new FileWriter(myFile, true)) {
+			Gson gson = new GsonBuilder().create();
+			gson.toJson(studentRegistered, writer);
+			writer.write("\n");
+			writer.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
+
+	}
+
+	public static void ReloadFile(HashMap<String, String[]> studentRegistered) {
+		File myFile = new File("student.json");
+
+		try (FileWriter writer = new FileWriter(myFile)) {
 			Gson gson = new GsonBuilder().create();
 			gson.toJson(studentRegistered, writer);
 			writer.write("\n");
@@ -82,7 +165,6 @@ public class SchoolManagement {
 		int numOfStudent = 0;
 		boolean ismore = true;
 		HashMap<String, String[]> studentRegistered = new HashMap<String, String[]>();
-		
 
 		System.out.println("In student Registeration, Enter the details of student");
 
@@ -110,42 +192,66 @@ public class SchoolManagement {
 				numOfStudent++;
 			} else {
 				ismore = false;
-				System.out.println("The total number of students you have enrolled is " + (numOfStudent + 1)
-						+ "and they are shown below");
+				System.out.println("The total number of students you have enrolled is " + (numOfStudent + 1));
 
 			}
-		
+
 		}
 		putInFile(studentRegistered);
 	}
 
-	public static void Searching(String id) {
-		File myFile = new File("student.json");
-		Gson gson = new GsonBuilder().create();
-		HashMap<String, String[]> allStudent = new HashMap();
-		HashMap<String, String[]> temp = new HashMap();
-
-		Scanner sc;
-		try {
-			sc = new Scanner(myFile);
-			while (sc.hasNextLine()) {
-				String st;
-				st = sc.nextLine();
-				Type type = new TypeToken<HashMap<String, String[]>>() {
-				}.getType();
-				temp = gson.fromJson(st, type);
-				allStudent.putAll(temp);
-			}
-
-		} catch (FileNotFoundException e) {
-
-			e.printStackTrace();
-		}
+	public static void Searching(String id, HashMap<String, String[]> allStudent) {
 
 		if (allStudent.containsKey(id)) {
-			System.out.print(id+" ");
-			for (int i=0; i<allStudent.get(id).length; i++)
-			System.out.print(allStudent.get(id)[i]+" ");
+			System.out.print(id + " ");
+			for (int i = 0; i < allStudent.get(id).length; i++)
+				System.out.print(allStudent.get(id)[i] + " ");
+		}
+	}
+
+	public static void delete(String id, HashMap<String, String[]> allStudent) {
+
+		if (allStudent.containsKey(id)) {
+			System.out.print(id + " ");
+			for (int i = 0; i < allStudent.get(id).length; i++) {
+				System.out.print(allStudent.get(id)[i] + " ");
+			}
+			allStudent.remove(id);
+			System.out.println(" Has been successfully deleted ");
+			ReloadFile(allStudent);
+
+		}
+	}
+
+	public static void update(String id, HashMap<String, String[]> allStudent) {
+		Scanner sc = new Scanner(System.in);
+		String[] information = new String[2];
+
+		if (allStudent.containsKey(id)) {
+			System.out.print(id + " ");
+			for (int i = 0; i < allStudent.get(id).length; i++) {
+				System.out.print(allStudent.get(id)[i] + " ");
+			}
+			System.out.println(" is ready to update");
+
+			information = allStudent.get(id);
+			System.out.println("What do you want to update ");
+			System.out.println("1) name ");
+			System.out.println("2) address ");
+			int choose = Integer.parseInt(sc.nextLine());
+
+			if (choose == 1) {
+				System.out.println("Write the name of student " + id);
+				information[0] = sc.nextLine();
+			}
+			if (choose == 2) {
+				System.out.println("Write the address " + id);
+				information[1] = sc.nextLine();
+			}
+			allStudent.put(id, information);
+			ReloadFile(allStudent);
+			System.out.print(id + " Has been successfully updated ");
+
 		}
 	}
 }
